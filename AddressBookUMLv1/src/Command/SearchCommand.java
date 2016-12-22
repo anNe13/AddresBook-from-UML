306a17f48a8ad08c49e8dd0693a7f340fff8867a
nikzin <nikolay.zinin@gmail.com>
2016-12-22 11:20:05 +0100

    search method

--- AddressBookUMLv1/src/Command/SearchCommand.java
+++ AddressBookUMLv1/src/Command/SearchCommand.java
@@ -21,7 +21,7 @@ public class SearchCommand implements Command {
         this.remoteRegistry = remoteRegistry;
         this.term=term;
     }
-
+//push
 
 
     public void execute() throws InvalidCommandParameterException {
